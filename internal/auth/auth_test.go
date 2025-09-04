470efe5187d0e61f8c66e44cfeb718229f83cf9b
tsi4456 <cremedefraise@gmail.com>
2025-09-04 11:31:02 +0100

    testing broken ci tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,37 @@
+package auth
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestAuth(t *testing.T) {
+	req, _ := http.NewRequest("GET", "blah", nil)
+	baseHeader := req.Header
+	missingAuthorisation := baseHeader.Clone()
+	hasAuthorisation := baseHeader.Clone()
+	hasAuthorisation.Add("Authorization", "ApiKey blahblahblah")
+	shortAuth := hasAuthorisation.Clone()
+	shortAuth.Set("Authorization", "ApiKeyHasNoSpaces")
+	wrongAuth := hasAuthorisation.Clone()
+	wrongAuth.Set("Authorization", "Ap1Key blahblahblah")
+
+	tests := []struct {
+		name     string
+		input    http.Header
+		want_str string
+	}{
+		{name: "no headers", input: missingAuthorisation, want_str: ""},
+		{name: "correct header format", input: hasAuthorisation, want_str: "blahblahblah"},
+		{name: "short header", input: shortAuth, want_str: ""},
+		{name: "wrong header", input: wrongAuth, want_str: ""},
+	}
+
+	for _, tc := range tests {
+		got, _ := GetAPIKey(tc.input)
+		if !reflect.DeepEqual(tc.want_str, got) {
+			t.Fatalf("%s: expected: %v, got: %v", tc.name, tc.want_str, got)
+		}
+	}
+}
